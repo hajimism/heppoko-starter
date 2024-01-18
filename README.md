b9a3b66f7bd20edcea16bd1449d308264fe33935
hajimism <h_nakagawa@yumemi.co.jp>
2024-01-18 10:03:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Heppoko Starter
+
+へっぽこUIハッカソンでプロトタイプを作る際に便利なスターターです。
+
+[Vite](https://vitejs.dev/) + React + TS + [shadcn/ui](https://ui.shadcn.com/) で構成されています。あらかじめいくつかのコンポーネントが入っています。
+
+```bash
+# 開発サーバーの起動
+nr dev
+
+# format + lint
+nr check
+```
+
+※コマンドは [antfu/ni](https://github.com/antfu/ni) を前提にしています。
---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Switch } from "@/components/ui/switch";
+import { Textarea } from "@/components/ui/textarea";
+
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex flex-col gap-8 justify-center items-center min-h-screen">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Form
+      </h1>
+      <div className="flex items-center space-x-2">
+        <Switch id="airplane-mode" />
+        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+      </div>
+
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms" />
+        <label
+          htmlFor="terms"
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          Accept terms and conditions
+        </label>
+      </div>
+
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="email">Email</Label>
+        <Input type="email" id="email" placeholder="Email" />
+      </div>
+
+      <Textarea className="max-w-md" placeholder="Type your message here." />
+
+      <RadioGroup defaultValue="comfortable">
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="default" id="r1" />
+          <Label htmlFor="r1">Default</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="comfortable" id="r2" />
+          <Label htmlFor="r2">Comfortable</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="compact" id="r3" />
+          <Label htmlFor="r3">Compact</Label>
+        </div>
+      </RadioGroup>
+
+      <Button>Submit</Button>
+    </main>
+  );
+}
+
+export default App;
